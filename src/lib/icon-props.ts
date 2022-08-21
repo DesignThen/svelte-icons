e67ddadef03f681d3f5206359806f266d0edd41a
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-08-21 14:28:07 +0100

    resolve missing type definitions from sveltettsx

--- src/lib/icon-props.ts
+++ src/lib/icon-props.ts
@@ -1,16 +1,16 @@
-const shared: svelte.JSX.SVGProps<SVGSVGElement> = {
+const shared: SVGProps<SVGSVGElement> = {
 	role: "img",
 	"aria-hidden": "true",
 	style: `width: auto; display: inline-block;`,
 	// viewBox: "0 0 24 24",
 };
 
-export const solid: svelte.JSX.SVGProps<SVGSVGElement> = {
+export const solid: SVGProps<SVGSVGElement> = {
 	...shared,
 	fill: "currentColor",
 };
 
-export const outline: svelte.JSX.SVGProps<SVGSVGElement> = {
+export const outline: SVGProps<SVGSVGElement> = {
 	...shared,
 	stroke: "currentColor",
 };
