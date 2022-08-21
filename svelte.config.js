f8f31d2481acbd4a40294d3bc08f51c989274ec2
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-08-21 12:45:11 +0100

    upgrade svelte-kit

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
-import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
-import path from "path";
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,15 +10,7 @@ const config = {
 	preprocess: [preprocess({})],
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		adapter: adapter({}),
-		vite: {
-			resolve: {
-				alias: {
-					$lib: path.resolve("src/lib"),
-				},
-			},
-		},
 	},
 };
 
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { sveltekit } from "@sveltejs/kit/vite";
+import path from "path";
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+
+	resolve: {
+		alias: {
+			$lib: path.resolve("src/lib"),
+		},
+	},
+};
+
+export default config;
