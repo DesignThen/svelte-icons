996c7dd1f14230d007d1cbbc62c2caacbaeb6cac
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2022-08-21 13:47:48 +0100

    fixes adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,6 +11,7 @@ const config = {
 
 	kit: {
 		adapter: adapter({}),
+		prerender: { default: true },
 	},
 };
 
