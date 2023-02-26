614bdc9a219319cf362d1afaa2580f4f05430a10
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-02-26 15:12:19 +0000

    update deploy configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: [".svelte"],
-
-	// Consult https://github.com/sveltejs/svelte-preprocess
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [preprocess({})],
+	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({}),
+		adapter: adapter(),
+		alias: {
+			$lib: "src/lib",
+		},
 	},
-	package: {},
 };
 
 export default config;
