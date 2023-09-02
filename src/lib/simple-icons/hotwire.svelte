376daa12960fd8363b627039da699bfaef7e160f
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2023-09-02 16:31:50 +0100

    add or update simple-icons

--- src/lib/simple-icons/hotwire.svelte
+++ src/lib/simple-icons/hotwire.svelte
@@ -6,7 +6,7 @@
 	role="img"
 	{...$$restProps}
 	fill="currentColor"
-	><title>PyTorch Lightning</title><path
-		d="M12 0L1.75 6v12L12 24l10.25-6V6zm-1.775 18l1.08-4.657-2.428-2.397L13.79 6l-1.082 4.665 2.414 2.384z"
+	><title>Hotwire</title><path
+		d="m16.764 6.917-3.48.81L16.32 0 7.236 11.705l4.334-.854-4.087 7.982 2.364-.532L7.456 24l7.51-8.111-2.77.64 4.568-9.612z"
 	/></svg
 >
