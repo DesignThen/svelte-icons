9fa1ce211bd93cbd79e3679329c822c060d942b5
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-01-18 16:29:03 +0000

    Add heroicons mini

--- scripts/clone-heroicons.sh
+++ scripts/clone-heroicons.sh
@@ -8,30 +8,34 @@ REPO="tailwindlabs/heroicons"
 REPO_FOLDER="optimized"
 REPO_branch="master"
 
-
 clone_and_move $name $REPO $REPO_FOLDER $REPO_branch
 
 ASSETS="$FOLDER/$name"
 
- echo "🚧 fixing folders..."
+echo "🚧 fixing folders..."
 
- MV_FROM="$ASSETS/20/solid"
- MV_TO="$FOLDER/heroicon-20-solid"
- mv $MV_FROM $MV_TO
- echo "✅ moved :: $MV_FROM to $MV_TO..."
+MV_FROM="$ASSETS/16/solid"
+MV_TO="$FOLDER/heroicon-16-solid"
+mv $MV_FROM $MV_TO
+echo "✅ moved :: $MV_FROM to $MV_TO..."
 
- MV_FROM="$ASSETS/24/solid"
- MV_TO="$FOLDER/heroicon-24-solid"
- mv $MV_FROM $MV_TO
- echo "✅ moved :: $MV_FROM to $MV_TO..."
+MV_FROM="$ASSETS/20/solid"
+MV_TO="$FOLDER/heroicon-20-solid"
+mv $MV_FROM $MV_TO
+echo "✅ moved :: $MV_FROM to $MV_TO..."
 
- MV_FROM="$ASSETS/24/outline"
- MV_TO="$FOLDER/heroicon-24-outline"
- mv $MV_FROM $MV_TO
- echo "✅ moved :: $MV_FROM to $MV_TO..."
+MV_FROM="$ASSETS/24/solid"
+MV_TO="$FOLDER/heroicon-24-solid"
+mv $MV_FROM $MV_TO
+echo "✅ moved :: $MV_FROM to $MV_TO..."
 
- rm -rf $ASSETS
- echo "✅ ...deleted empty folders."
+MV_FROM="$ASSETS/24/outline"
+MV_TO="$FOLDER/heroicon-24-outline"
+mv $MV_FROM $MV_TO
+echo "✅ moved :: $MV_FROM to $MV_TO..."
+
+rm -rf $ASSETS
+echo "✅ ...deleted empty folders."
 #
 # echo "✅ done :: "
 
