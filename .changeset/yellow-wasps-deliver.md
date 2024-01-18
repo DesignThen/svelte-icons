18600c977506d48fb647dbe787550b3b0e881977
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-01-18 17:07:37 +0000

    Update SimpleIcons

---
+++ .changeset/yellow-wasps-deliver.md
@@ -0,0 +1,5 @@
+---
+"@inqling/svelte-icons": patch
+---
+
+Added 334 new SimpleIcons
---
+++ src/lib/simple-icons/stockx.svelte
@@ -0,0 +1,12 @@
+<svg
+	viewBox="0 0 24 24"
+	xmlns="http://www.w3.org/2000/svg"
+	style="display: inline-block;"
+	aria-hidden="true"
+	role="img"
+	{...$$restProps}
+	fill="currentColor"
+	><title>StockX</title><path
+		d="M13.74 16.5 22.5 24v-6l-7-6 7-6V0L10.26 10.5v-3L1.5 0v6l7 6-7 6v6l12.24-10.5Z"
+	/></svg
+>
