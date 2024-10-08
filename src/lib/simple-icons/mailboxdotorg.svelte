66c4c0e0a670145455192589fba04f91f672dd20
Ollie Taylor <13766232+OllieJT@users.noreply.github.com>
2024-10-08 13:34:29 +0100

    corrects pnpm version in CI (#59)

---
+++ src/lib/simple-icons/mailboxdotorg.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	const { ...restProps } = $props<{ [x: string]: unknown }>();
+</script>
+
+<svg
+	viewBox="0 0 24 24"
+	xmlns="http://www.w3.org/2000/svg"
+	style="display: inline-block;"
+	aria-hidden="true"
+	role="img"
+	{...restProps}
+	fill="currentColor"
+	><title>mailbox.org</title><path
+		d="M2.229 22.844H24V10.501l-8.628 5.882c-.613.419-1.226-.003-1.226-.003L0 6.646v13.969s0 2.229 2.229 2.229m12.558-9.273L24 7.261V1.156H2.229S0 1.156 0 3.384v.06l14.787 10.127Z"
+	/></svg
+>
